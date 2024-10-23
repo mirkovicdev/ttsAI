55d8bac719046f10342c78f68cb8a3bbe6306c55
Antonije Mirkovic <antonije@mirkovic.no>
2024-10-23 20:07:17 +0200

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig =  {
+  providers: [
+    {
+      domain: "https://loyal-whale-14.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "lovely-flamingo-139.convex.cloud"
+      },
+      {
+        protocol: 'https',
+        hostname: "energetic-octopus-36.convex.cloud"
+      },
+      {
+        protocol: 'https',
+        hostname: "lovely-flamingo-139.convex.cloud(*)"
+      },
+      {
+        protocol: 'https',
+        hostname: "img.clerk.com"
+      },
+    ]
+  }
+};
 
 export default nextConfig;
